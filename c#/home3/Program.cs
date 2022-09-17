c4ba33f26c228130db7cd2debb3c95fb3b1ccf91
Vihnovskaya Vika <vihnovskaya1932@gmail.com>
2022-09-17 17:38:48 +0300

    Программа проверки четности числа

---
+++ c#/home3/Program.cs
@@ -0,0 +1,8 @@
+﻿// Напишите программу, которая на вход принимает число и выдает, является ли число чётным
+Console.WriteLine("Введите число");
+int number = Convert.ToInt32(Console.ReadLine());
+if (number%2== 0) 
+{
+    Console.WriteLine (" Число чётное") ;
+}
+else Console.WriteLine (" Число не является чётным");

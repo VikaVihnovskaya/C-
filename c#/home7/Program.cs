460c8b6e7ec4c683bcc1245eaf53b26ae34789cd
Vihnovskaya Vika <vihnovskaya1932@gmail.com>
2022-09-26 17:42:57 +0300

    Программа рассчета растояния между двумя точками в 3д пространстве

---
+++ c#/home7/Program.cs
@@ -0,0 +1,15 @@
+﻿// Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
+Console.Write("Введите координату х1 для первой точки: ");
+int x1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите координату y1 для первой точки: ");
+int y1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите координату z1 для первой точки: ");
+int z1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите координату x2 для второй точки: ");
+int x2 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите координату y2 для второй точки: ");
+int y2 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите координату z2 для второй точки: ");
+int z2 = Convert.ToInt32(Console.ReadLine());
+double l = Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2) + Math.Pow(z2 - z1, 2));
+Console.WriteLine($"Расстояние между двумя точками в 3d пространстве равно {l}");

c703569a87cc22f078639542518dd00b600980f3
Vihnovskaya Vika <vihnovskaya1932@gmail.com>
2022-09-26 19:52:12 +0300

    Программа возведения числа N в куб

---
+++ c#/home8/Program.cs
@@ -0,0 +1,10 @@
+﻿// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+Console.Write("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Результат кубов чисел: ");
+for(int i= 1; i<=num; i++)
+{
+   double number = Math.Pow (Convert.ToDouble(i),3.0);
+   Console.WriteLine(Convert.ToInt32(number));
+}

bbe0241f3a1237b777432e5eda927fc7bb4c8462
Vihnovskaya Vika <vihnovskaya1932@gmail.com>
2022-09-28 16:53:20 +0300

    Цикл на возведение числа в его натуральную степень

---
+++ c#/homework/Program.cs
@@ -0,0 +1,17 @@
+﻿// Задача 25. Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+Console.WriteLine("Введите число А");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число В");
+int b = Convert.ToInt32(Console.ReadLine());
+int num = 1;
+int pow = 1;
+while( pow <= b)
+{
+    num=num * a;
+    pow++;
+    
+}
+Console.WriteLine($"Ответ:{num}");
+
+
+
